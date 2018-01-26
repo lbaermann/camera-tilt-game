cfb1ddd2605a7b7735609a3c98087673fc296e8c
Leonard <lbaermann@hotmail.de>
2018-01-26 22:50:57 +0100

    Use median instead of avg, add morphologic open operation

--- src/app/image-processor.service.ts
+++ src/app/image-processor.service.ts
@@ -17,23 +17,35 @@ export class ImageProcessorService {
 
     this.convertToBlackWhite(data);
 
-    for (let row = 1; row < image.width - 1; row++) {
-      for (let col = 1; col < image.height - 1; col++) {
-        const index = this.getPixelIndex(image, row, col);
-        const above = this.getPixelIndex(image, row - 1, col);
-        const below = this.getPixelIndex(image, row + 1, col);
-        const left = this.getPixelIndex(image, row, col - 1);
-        const right = this.getPixelIndex(image, row, col + 1);
-
-        /*if (above && below && left && right) {
-          this.setPixel(image.data, index, 255);
-        } else {
-          this.setPixel(image.data, index, 0);
-        }*/
-      }
+    for (let i = 0; i < 1; i++) {
+      this.morphologicOp(image, (top, right, bottom, left) => top || right || bottom || left);
+      this.morphologicOp(image, (top, right, bottom, left) => top && right && bottom && left);
     }
 
     return image;
+  }
+
+  private morphologicOp(image: Image, functor: (top: number, right: number, bottom: number, left: number) => number) {
+    const imgCopy: Image = {
+      data: new Buffer(image.data),
+      width: image.width,
+      height: image.height
+    };
+    for (let row = 1; row < image.width - 1; row++) {
+      for (let col = 1; col < image.height - 1; col++) {
+        const index = this.getPixelIndex(image, row, col);
+        const above = this.getPixel(imgCopy, row - 1, col);
+        const below = this.getPixel(imgCopy, row + 1, col);
+        const left = this.getPixel(imgCopy, row, col - 1);
+        const right = this.getPixel(imgCopy, row, col + 1);
+
+        if (above || below || right || left) {
+          this.setPixel(image.data, index, 255);
+        } else {
+          this.setPixel(image.data, index, 0);
+        }
+      }
+    }
   }
 
   private getPixel(image: Image, row: number, col: number): number {
@@ -45,16 +57,26 @@ export class ImageProcessorService {
   }
 
   private convertToBlackWhite(data: Buffer) {
-    let sum = 0;
-    for (let i = 0; i < data.byteLength; i++) {
-      sum += data[i];
-    }
-    const avg = sum / data.byteLength;
+    const avg = this.calcMedian(data);
     for (let i = 0; i < data.byteLength; i += 4) {
       const localAvg = (data[i] + data[i + 1] + data[i + 2] + data[i + 3]) / 4;
       const resultingPixel = localAvg >= avg ? 255 : 0;
       this.setPixel(data, i, resultingPixel);
     }
+  }
+
+  /*private calcAvg(data: Buffer) {
+    let sum = 0;
+    for (let i = 0; i < data.byteLength; i++) {
+      sum += data[i];
+    }
+    return sum / data.byteLength;
+  }*/
+
+  private calcMedian(data: Buffer) {
+    const buffer = new Buffer(data);
+    buffer.sort();
+    return buffer[buffer.byteLength / 2];
   }
 
   private setPixel(data: Buffer, i: number, resultingPixel: number) {
